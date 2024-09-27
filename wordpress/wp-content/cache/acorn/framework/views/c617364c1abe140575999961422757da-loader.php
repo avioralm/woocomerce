cf5661d08f446e4ed555ad81fb1781286d990e39
Bob <bob@Bobs-MBP.lan>
2024-09-27 16:49:51 +0300

    remove old theme

--- wordpress/wp-content/cache/acorn/framework/views/c617364c1abe140575999961422757da-loader.php
+++
@@ -1,2 +0,0 @@
-<?= \Roots\view('woocommerce.single-product.add-to-cart.simple', $data ?? get_defined_vars())->render(); ?>
-<?php /**PATH /var/www/html/wp-content/themes/rideped/resources/views/woocommerce/single-product/add-to-cart/simple.blade.php ENDPATH**/ ?>
